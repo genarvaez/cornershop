fdd370b6fcd95ed33c9bb4f1929b08dde05dfc7a
genarvaez <ge_naravez@hotmail.com>
2017-08-30 05:10:22 -0300

    union de componentes, validador de direccion y funcionalidad de google maps

--- dist/js/script.js
+++ dist/js/script.js
@@ -26864,3 +26864,15 @@ function geocodeAddress(geocoder, resultsMap) {
     }
 }
 
+
+
+$(document).ready(function(){
+    $('#btn-continuar').click(function() {
+        //Se verifica si la opcion del select esta vacia
+        if ($('#selector').val().trim() ==! '') {
+            $('.seccion-modal').show();
+        } else {
+           $('.seccion-modal').hide();
+        }
+    });
+});
